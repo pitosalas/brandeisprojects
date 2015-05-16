3221166edf396fab1a5d6ee7fce14362def2c4fc
Pito Salas <pitosalas@gmail.com>
2015-05-16 16:37:13 -0400

    debugging some odd problems with _posts

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('#projects').dataTable( {
-        "paging":   false,
         "columnDefs": [
           { "width": "50%", "targets": 1 },
           { "width": "3%", "targets": [ 2, 3, 4 ]}
-          ]
+          ], 
+        "paging":   false
     } );
-} );
+}
