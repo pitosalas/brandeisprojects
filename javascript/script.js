cac23cb181de115d4929ed38e029b1952c8f66c3
Pito Salas <pitosalas@gmail.com>
2016-05-12 14:56:59 -0400

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('#projects').dataTable( {
         "columnDefs": [
-          { "width": "50%", "targets": 1 },
-          { "width": "3%", "targets": [ 2, 3, 4 ]}
-          ], 
+          { "width": "50%", "targets": 3 },
+          { "width": "3%", "targets": [ 4, 5, 6 ]}
+          ],
         "paging":   false
     } );
 });
